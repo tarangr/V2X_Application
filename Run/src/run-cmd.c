8ebc5b13ea6d0a963e557f66bae020f7e71289ea
tarang <tarangp@danlawinc.com>
2018-11-21 13:40:58 +0530

    updated with v1.2

--- Run/src/run-cmd.c
+++ Run/src/run-cmd.c
@@ -12,8 +12,9 @@ sync \n\
 int main()
 {	
 	//int fd;
-	//char pm_hm_SSL[50] = "SSL_ID 2 System Going to Operate Mode\n";
-	system("/usr/bin/run_cmd_nyc.sh");
+		//char pm_hm_SSL[50] = "SSL_ID 2 System Going to Operate Mode\n";
+		printf("in run\n");
+		system("/usr/bin/run_cmd_nyc.sh");
 	/*fd = open("tmpSSL.txt",O_CREAT|O_RDWR|O_APPEND,0777);
 	if(fd == -1)
         {
